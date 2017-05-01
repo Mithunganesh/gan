7e9c3f295b263d88a72aa79d98dd875d19ade5df
Mithunganesh <mithunganesh1@gmail.com>
2017-05-01 13:51:44 +0530

    Create prog2.java

---
+++ prog2.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class prog2
+{
+public static void main(String ar[])
+{
+Scanner s=new Scanner(System.in);
+System.out.println("enter number");
+int n=s.nextInt();
+if(n%2==0){
+System.out.println("even");}
+else{
+System.out.println("odd");}
+}
+}
